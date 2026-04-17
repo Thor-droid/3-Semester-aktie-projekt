6fd32ae537db2124befe23ee9d1e2b2699cade83
Thor-droid <10512525@ucn.dk>
2026-04-17 10:15:03 +0200

    Tilføjet MVC

---
+++ Aktie_loginForm/Aktie_WebsiteMVCV2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aktie_WebsiteMVCV2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
