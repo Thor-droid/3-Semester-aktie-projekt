00828dd2b64a7a51bebe29aa7abd396253fa9676
Thor-droid <10512525@ucn.dk>
2026-04-22 12:11:42 +0200

    Tilføjet Register

---
+++ Aktie_loginForm/UnitTests/RegisterTest.cs
@@ -0,0 +1,40 @@
+﻿using Aktie_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace UnitTests
+{
+    public class RegisterTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public RegisterTest(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task TestRegister()
+        {
+            // Arrange
+            var model = new RegisterModel
+            {
+                Email = "test" + Guid.NewGuid() + "@mail.com",
+                KundeNavn = "UnitTestUser",
+                Password = "123456"
+            };
+
+            // Act
+            var response = await _client.PostAsJsonAsync("api/auth/register", model);
+
+            // Assert
+            Assert.True(
+                response.StatusCode == HttpStatusCode.OK ||
+                response.StatusCode == HttpStatusCode.BadRequest
+            );
+        }
+    }
+}
