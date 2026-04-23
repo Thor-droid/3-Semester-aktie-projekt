4c5e0bcbdda49f26e9dee608cf75900ecfd7e2cd
Bastianhj1312 <10665995@ucn.dk>
2026-04-23 12:50:34 +0200

    tilføjet abonnement, men det skal stadig finpudses

---
+++ Aktie_loginForm/Aktie_WebsiteMVCV2/Controllers/AbonnementController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aktie_WebsiteMVCV2.Controllers
+{
+    [Authorize]
+    public class AbonnementController : Controller
+    {
+        [HttpGet]
+        public IActionResult Abonnement()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Buy(string packageName, string price)
+        {
+            TempData["SuccessMessage"] =
+                $"Køb simuleret: Du har valgt pakken {packageName} til {price}.";
+
+            return RedirectToAction("Abonnement");
+        }
+    }
+}
