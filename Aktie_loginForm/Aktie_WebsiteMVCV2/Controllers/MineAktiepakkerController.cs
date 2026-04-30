2ffeeb0ec1fb57435e75e099ba58f66f38f22652
Bastianhj1312 <10665995@ucn.dk>
2026-04-30 13:52:31 +0200

    Tilføjet, MineAktiepakker side
    til at se de pakker man er abonneeret til

---
+++ Aktie_loginForm/Aktie_WebsiteMVCV2/Controllers/MineAktiepakkerController.cs
@@ -0,0 +1,63 @@
+﻿using Aktie_WebsiteMVCV2.DTO.Abonnement;
+using Aktie_WebsiteMVCV2.DTO.Stock;
+using Aktie_WebsiteMVCV2.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aktie_WebsiteMVCV2.Controllers
+{
+    [Authorize]
+    public class MineAktiepakkerController : Controller
+    {
+        private readonly AbonnementApiService _abonnementService;
+        private readonly StockApiService _stockService;
+
+        public MineAktiepakkerController(
+            AbonnementApiService abonnementService,
+            StockApiService stockService)
+        {
+            _abonnementService = abonnementService;
+            _stockService = stockService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var kundeIdClaim = User.FindFirst("KundeId");
+
+            if (kundeIdClaim == null)
+                return RedirectToAction("Login", "Account");
+
+            int kundeId = int.Parse(kundeIdClaim.Value);
+
+            var abonnement = await _abonnementService.GetByCustomer(kundeId);
+
+            if (abonnement == null)
+                return View(null);
+
+            List<string> symbols = abonnement.KategoriId switch
+            {
+                1 => new List<string> { "AAPL", "MSFT", "TSLA", "GOOGL", "AMZN" },
+                2 => new List<string> { "AAPL", "MSFT", "TSLA", "GOOGL", "AMZN", "NVDA", "META", "NFLX", "AMD", "INTC" },
+                3 => new List<string> { "AAPL", "MSFT", "TSLA", "GOOGL", "AMZN", "NVDA", "META", "NFLX", "AMD", "INTC", "IBM", "ORCL", "DIS", "V", "BABA" },
+                _ => new List<string>()
+            };
+
+            var stocks = new List<GlobalQuoteDto>();
+
+            foreach (var symbol in symbols)
+            {
+                var stock = await _stockService.GetStock(symbol);
+
+                if (stock != null)
+                    stocks.Add(stock);
+
+                await Task.Delay(500);
+            }
+
+            var model = new Tuple<AbonnementResponse, List<GlobalQuoteDto>>(abonnement, stocks);
+
+            return View(model);
+        }
+    }
+}
