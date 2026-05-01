966e915089092bcff8bf396d1b0e2c639589d799
Thor-droid <10512525@ucn.dk>
2026-05-01 12:22:16 +0200

    Lavet lidt pænere ariketur og begyndt på DesktopClient

---
+++ Aktie_loginForm/Aktie_WebsiteMVCV2/Interface/ILoginService.cs
@@ -0,0 +1,5 @@
+﻿public interface ILoginService
+{
+    Task SignInUser(HttpContext httpContext, LoginResponse result);
+    Task SignOutUser(HttpContext httpContext);
+}
---
+++ Aktie_loginForm/Aktie_loginForm/Model/Customer.cs
@@ -0,0 +1,12 @@
+﻿public class Customer
+{
+    public int KundeId { get; set; }
+
+    public string Navn { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public bool IsAdmin { get; set; }
+}
